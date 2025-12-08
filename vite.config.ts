33672fdeb84c194b8f8b4735a67787f882b3e25b
MYKHIL <darkmic50@gmail.com>
2025-12-08 23:05:36 +0000

    Configure deployment for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,19 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       }
-    }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          pdfGenerator: path.resolve(__dirname, 'services/pdfGenerator.ts')
+        },
+        output: {
+          manualChunks: {
+            // 'vendor-pdf': ['jspdf', 'html2canvas'], // Keep this off for now, let auto-splitting work
+          }
+        }
+      }
+    },
   };
 });
